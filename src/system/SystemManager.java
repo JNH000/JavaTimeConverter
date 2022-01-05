8a0fad7a8ecfc5bc5976ab94a0e9b053a519f413
JNH000 <jonahtestmail026@gmail.com>
2022-01-05 11:28:10 +0100

    Cleanup & Basic GUI
    Removed Main Class
    Added basic project structure
    Added GUI setup (Interface.java)

---
+++ src/system/SystemManager.java
@@ -0,0 +1,16 @@
+package system;
+
+import ui.Interface;
+
+public class SystemManager {
+
+    public static final int APP_WIDTH = 800;
+    public static final int APP_HEIGHT = 600;
+
+    private Interface ui = new Interface();
+
+    public static void main(String[] args){
+        SystemManager sysMan = new SystemManager();
+    }
+
+}
---
+++ src/ui/Interface.java
@@ -0,0 +1,42 @@
+package ui;
+
+import system.SystemManager;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+
+public class Interface {
+
+    public static Color uiColor = new Color(0x1e2733);
+    public static Color textColor = new Color(0x7ca296);
+
+    private static String version = "v.0.0.1";
+
+    private JFrame jFrame = new JFrame();
+    private JPanel jPanel = new JPanel();
+    private JLabel uiText = new JLabel("10:46am", SwingConstants.CENTER);
+
+
+    public Interface(){
+        jFrame.setLayout(new GridBagLayout());
+        jFrame.getContentPane().setBackground(uiColor);
+
+        jFrame.setTitle("Java Time Converter" + version);
+
+        uiText.setFont(new Font("Verdana", 1, 50));
+        uiText.setForeground(textColor);
+        jPanel.setOpaque(false);
+        jPanel.add(uiText);
+        jFrame.add(jPanel);
+
+        jFrame.setSize(400,400);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.setResizable(false);
+        jFrame.setVisible(true);
+    }
+}
